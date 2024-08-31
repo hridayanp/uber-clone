b84fd8a54a3d159d64739376651e44ac02af423e
hridayanp <hridayanphukan123@gmail.com>
2024-08-31 22:56:11 +0530

    initial project setup

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+
+const home = () => {
+  return (
+    <SafeAreaView>
+      <Text>index</Text>
+    </SafeAreaView>
+  );
+};
+
+export default home;
